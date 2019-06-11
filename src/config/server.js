524937478cffd234185c8728ab4645acbeb86734
Augusto Scher <augustoscher@gmail.com>
2019-06-11 08:31:00 -0300

    project setup

--- src/config/server.js
+++ src/config/server.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
-server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 server.listen(port, function() {
-    console.log(`Backend running on port ${port}`)
+    console.log(`Backend running on port ${port}.`)
 })
